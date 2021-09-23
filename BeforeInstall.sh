954989b2bfa9ec5d96488f88723bb1bc8a173b4e
rahul <rahulbali@gmail.com>
2021-09-23 10:39:32 +0530

    check please wor

--- BeforeInstall.sh
+++ BeforeInstall.sh
@@ -1,18 +1,3 @@
-
-sudo apt install ruby-full
-cd /home/ubuntu
-sudo wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-sudo chmod +x ./install
-sudo chmod +x ./install
-sudo ./install auto
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo systemctl start apache2.service
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install php7.1
-sudo apt-get install php7.1 php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-mbstring php7.1-mcrypt php7.1-zip php7.1-imagick  -y
-sudo service apache2 restart
-
+sudo yum update
+sudo yum install httpd -y
 
